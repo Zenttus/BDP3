fc88927d744e4c64425c1aa62a06a968e68580ec
Pedro Santiago <pedro.santiago16@upr.edu>
2018-12-09 19:46:55 -0400

    attempt

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     #manager = HDFSManager()
     #tweets_catcher.get_tweets(manager)
 
-    model = TextClassifierModel1('cleantextlabels7.csv', dict='./dict.txt')
+    model = TextClassifierModel1('cleantextlabels7.csv', dictionary='./dict.txt')
     model.initiate_model()
